7375d63a21eb8bdf775eb200306e84742832e1ac
YahyoToirov <yahyotoirov7@gmail.com>
2024-09-04 10:59:44 +0500

    site

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      orange: ["#FD695A"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
